f5ee69d69d1d7cfa17f169753643b641bcedf416
Pastukhov A <pactx@yandex.ru>
2020-06-30 14:48:25 +0300

    oauth

---
+++ oauth/oauth.go
@@ -0,0 +1,91 @@
+package oauth
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pastukhov-aleksandr/bookstore_aouth_api/utils/secret_code"
+	"github.com/pastukhov-aleksandr/bookstore_utils-go/rest_errors"
+)
+
+const (
+	paramAccessToken = "access_token"
+)
+
+func AuthenticateRequest(request *http.Request) (int64, rest_errors.RestErr) {
+	if request == nil {
+		return 0, rest_errors.NewUnauthorizedError("unauthorized")
+	}
+
+	tokenAuth, err := extractTokenMetadata(request)
+	if err != nil {
+		return 0, rest_errors.NewUnauthorizedError("unauthorized")
+	}
+}
+
+func extractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+func verifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := extractToken(r)
+	access_sicret := secret_code.Get_ACCESS_SECRET()
+	if access_sicret == "" {
+		return nil, fmt.Errorf("invalid env file")
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(access_sicret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func tokenValid(r *http.Request) error {
+	token, err := verifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+func extractTokenMetadata(r *http.Request) (*AccessDetails, error) {
+	token, err := verifyToken(r)
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUuid, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return &AccessDetails{
+			AccessUuid: accessUuid,
+			UserId:     userId,
+		}, nil
+	}
+	return nil, err
+}
