128ac4c72b03c04275106e478f876f81f3a72698
Pastukhov A <pactx@yandex.ru>
2020-06-30 15:07:27 +0300

    secret_code

---
+++ secret_code/secret_code.go
@@ -0,0 +1,25 @@
+package secret_code
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/pastukhov-aleksandr/bookstore_utils-go/getenv"
+)
+
+const (
+	ACCESS_SECRET  = "ACCESS_SECRET"
+	REFRESH_SECRET = "REFRESH_SECRET"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("invalid access token")
+	}
+}
+
+func Get_ACCESS_SECRET() string {
+	return getenv.GetEnv(ACCESS_SECRET, "")
+}
+
+func Get_REFRESH_SECRET() string {
+	return getenv.GetEnv(REFRESH_SECRET, "")
+}
