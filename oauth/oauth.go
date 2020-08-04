835dd974102d901abe074ca173e9fe10910f49cd
Pastukhov A <pactx@yandex.ru>
2020-08-04 16:28:18 +0300

    v1

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 	"github.com/pastukhov-aleksandr/bookstore_utils-go/rest_errors"
 	"github.com/pastukhov-aleksandr/bookstore_utils-go/secret_code"
 )
@@ -20,12 +21,37 @@ type AccessDetails struct {
 	ClientID int64
 }
 
-func AuthenticateRequest(request *http.Request) (*AccessDetails, rest_errors.RestErr) {
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := tokenValid(c.Request, secret_code.Get_ACCESS_SECRET())
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func TokenRefreshMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := tokenValid(c.Request, secret_code.Get_REFRESH_SECRET())
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func AuthenticateRequest(request *http.Request, secretCode string) (*AccessDetails, rest_errors.RestErr) {
 	if request == nil {
 		return nil, rest_errors.NewUnauthorizedError("unauthorized")
 	}
 
-	tokenAuth, err := extractTokenMetadata(request)
+	tokenString := extractToken(request)
+	tokenAuth, err := ExtractTokenMetadata(tokenString, secretCode)
 	if err != nil {
 		return nil, rest_errors.NewUnauthorizedError("unauthorized")
 	}
@@ -43,10 +69,8 @@ func extractToken(r *http.Request) string {
 	return ""
 }
 
-func verifyToken(r *http.Request) (*jwt.Token, error) {
-	tokenString := extractToken(r)
-	accessSicret := secret_code.Get_ACCESS_SECRET()
-	if accessSicret == "" {
+func verifyToken(tokenString string, secretCode string) (*jwt.Token, error) {
+	if secretCode == "" {
 		return nil, fmt.Errorf("invalid env file")
 	}
 
@@ -55,16 +79,19 @@ func verifyToken(r *http.Request) (*jwt.Token, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte(accessSicret), nil
+		return []byte(secretCode), nil
 	})
+
 	if err != nil {
 		return nil, err
 	}
 	return token, nil
 }
 
-func tokenValid(r *http.Request) error {
-	token, err := verifyToken(r)
+func tokenValid(r *http.Request, secretCode string) error {
+	tokenString := extractToken(r)
+	token, err := verifyToken(tokenString, secretCode)
+
 	if err != nil {
 		return err
 	}
@@ -74,12 +101,13 @@ func tokenValid(r *http.Request) error {
 	return nil
 }
 
-func extractTokenMetadata(r *http.Request) (*AccessDetails, error) {
-	token, err := verifyToken(r)
+func ExtractTokenMetadata(tokenString string, secretCode string) (*AccessDetails, error) {
+	token, err := verifyToken(tokenString, secretCode)
 	if err != nil {
 		return nil, err
 	}
 	claims, ok := token.Claims.(jwt.MapClaims)
+
 	if ok && token.Valid {
 		userId, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
 		if err != nil {
